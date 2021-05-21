bc01103d1b44c8e6b41404c7badbb72777cbd458
ambiguousname <tyly04@gmail.com>
2021-05-21 09:37:37 -0700

    Enemies can now fall into traps

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,14 @@
-// Forgive me for all of the ugly tricks I've had to use to shorten the number of lines this has. I could just minify, but this at least allows me to read my code.
+// Forgive me for all of the ugly tricks I've had to use to shorten the number of lines this has. I could just minify, but this at least allows me to somewhat read my code.
 var paths = new Map();
+var enemies = []; var obstacles = [];
 class Enemy {
 	constructor(enemyName, enemyType, enemyInstance) {
 		this.name = enemyName;
 		this.type = enemyType;
 		this.instance = enemyInstance;
 		this.pathIndex = 0;
+		this.dying = false;
+		this.trapImmunity = [];
 	}
 	initializePathing(){
 		this.instance.behaviors.MoveTo.addEventListener("arrived", () => this.updatePath(this));
@@ -16,27 +19,32 @@ class Enemy {
 		self.pathIndex += 1;
 		if (self.pathIndex >= paths[self.name].length) self.pathIndex = 0;
 	}
+	update(self){
+		if(self.instance.width <= 0 && self.instance.height <= 0) self.instance.destroy();
+		if (self.dying) {self.instance.width -= 2; self.instance.height -= 2; self.instance.behaviors.MoveTo.stop();}
+	}
+	getCollision(self, trapInstance){
+		if (!self.dying && self.instance.x > trapInstance.x - trapInstance.width/2 && self.instance.x < trapInstance.x + trapInstance.width/2 && self.instance.y > trapInstance.y - trapInstance.height/2 && self.instance.y < trapInstance.y + trapInstance.height/2 && !(trapInstance.objectType.name in self.trapImmunity)){
+			if(trapInstance.objectType.name === "Pitfall") self.dying = true;
+		}
+	}
 }
 runOnStartup(async runtime => runtime.addEventListener("beforeprojectstart", () => OnBeforeProjectStart(runtime)));
 async function OnBeforeProjectStart(runtime)
 {
-	// Get the paths and add them to our object:
-	var path_temp_arr = runtime.objects.PathObj.getAllInstances();
-	path_temp_arr.forEach(function(p){
+	runtime.objects.PathObj.getAllInstances().forEach(function(p){
 		// Okay, arrays probably weren't meant to be constructed this way.
 		var enemyName = p.instVars.EnemyName;
-		if (enemyName in paths) {
-			// We store the x and y coordinates of the path as a small array.
-			paths[enemyName][p.instVars.Position] = [p.x, p.y];
-		} else {
+		if (enemyName in paths) paths[enemyName][p.instVars.Position] = [p.x, p.y];
+		else {
 			paths[enemyName] = [];
 			paths[enemyName][p.instVars.Position] = [p.x, p.y];
 		}
 	});
-	// Now we get the enemies:
 	runtime.objects.Enemy.getAllInstances().forEach(function(e){
 		var enemy = new Enemy(e.instVars.EnemyName, e.instVars.EnemyType, e);
 		enemy.initializePathing();
+		enemies.push(enemy);
 	});
 	runtime.globalVars.Selected = 0;
 	var inv = runtime.objects.Inventory.getFirstInstance().instVars;
@@ -53,8 +61,10 @@ function Tick(runtime){
 		}
 	}
 	if (runtime.keyboard.isKeyDown("Space") && inv[Object.keys(inv)[runtime.globalVars.Selected]] > 0) {
-		runtime.objects[Object.keys(inv)[runtime.globalVars.Selected]].createInstance(0, runtime.objects.Player.getFirstInstance().x, runtime.objects.Player.getFirstInstance().y);
+		var trap = runtime.objects[Object.keys(inv)[runtime.globalVars.Selected]].createInstance(0, runtime.objects.Player.getFirstInstance().x, runtime.objects.Player.getFirstInstance().y);
+		obstacles.push(trap);
 		inv[Object.keys(inv)[runtime.globalVars.Selected]] -= 1;
 		runtime.objects.SelectionText.getFirstInstance().text = "Trap: " + Object.keys(inv)[runtime.globalVars.Selected] + ". Count: " + inv[Object.keys(inv)[runtime.globalVars.Selected]] + ".";
 	}
+	enemies.forEach((e) => {e.update(e); obstacles.forEach((o)=> e.getCollision(e, o));});
 }
