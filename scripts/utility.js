73e663ab19cf85ee84ce7753b296cb0a8ad0a331
ambiguousname <tyly04@gmail.com>
2021-05-29 20:44:24 -0700

    Added destroyer enemy

--- scripts/utility.js
+++ scripts/utility.js
@@ -1,22 +1,14 @@
 
 var paths = new Map(); var enemies = []; var obstacles = [];
-export {Enemy, Jumper, Digger, paths, enemies, obstacles};
+export {Enemy, Jumper, Digger, Destroyer, paths, enemies, obstacles};
 class Enemy {
-	constructor(enemyName, enemyInstance, index) {
-		this.name = enemyName; this.instance = enemyInstance; this.pathIndex = 0; this.dying = false; this.dead = false; this.index = index;
-		this.trapImmunity = [];
-	}
+	constructor(enemyName, enemyInstance, index) { this.name = enemyName; this.instance = enemyInstance; this.pathIndex = 0; this.dying = false; this.dead = false; this.index = index; this.trapImmunity = [];}
 	initializePathing(){ this.instance.behaviors.MoveTo.addEventListener("arrived", () => this.updatePath(this)); this.instance.behaviors.MoveTo.moveToPosition(paths[this.name][this.pathIndex][0], paths[this.name][this.pathIndex][1]);
 	}
 	updatePath(self){
 		if (!self.dying && !self.dead && !self.immobile){self.instance.behaviors.MoveTo.moveToPosition(paths[self.name][self.pathIndex][0], paths[self.name][self.pathIndex][1]); self.pathIndex += 1;
-			if (self.pathIndex >= paths[self.name].length) self.pathIndex = 0;}
-	}
-	update(self){
-		if (!self.dead){
-			if (self.dying) self.dyingUpdate(self, self.killer);
-		}
-	}
+			if (self.pathIndex >= paths[self.name].length) self.pathIndex = 0;}}
+	update(self){if (!self.dead){if (self.dying) self.dyingUpdate(self, self.killer);}}
 	kill(self){ self.dead = true; enemies.splice(self.index, 1); self.instance.destroy();}
 	dyingUpdate(self){
 		switch (self.killer.objectType.name) {
@@ -35,26 +27,17 @@ class Enemy {
 	}
 	getCollision(self, trapInstance){
 		if (!self.dying && trapInstance.objectType.name != "Block" && (trapInstance.isOpen === true) && !self.trapImmunity.includes(trapInstance.objectType.name) && self.instance.testOverlap(trapInstance)) {
-			self.dying = true; self.killer = trapInstance; self.instance.behaviors.MoveTo.moveToPosition(trapInstance.x, trapInstance.y); self.instance.behaviors.MoveTo.rotateSpeed = 0;
-		}
-	}
-}
+			self.dying = true; self.killer = trapInstance; self.instance.behaviors.MoveTo.moveToPosition(trapInstance.x, trapInstance.y);}}}
 class Jumper extends Enemy {
 	constructor(enemyName, enemyInstance){
 		super(enemyName, enemyInstance); this.trapImmunity = ["Pitfall", "Trapdoor"]; this.jumpTimer = 0; this.landTimer = 0;
 	}
 	update(self){
-		if (!self.dead) {if (self.dying) self.dyingUpdate(self, self.killer);} if(self.jumpTimer > 0) {self.jumpTimer -= self.instance.runtime.dt; if (self.jumpTimer <= 0) self.instance.zElevation -= 30; } else {self.landTimer += self.instance.runtime.dt;}
-	}
+		if (!self.dead) {if (self.dying) self.dyingUpdate(self, self.killer);} if(self.jumpTimer > 0) {self.jumpTimer -= self.instance.runtime.dt; if (self.jumpTimer <= 0) self.instance.zElevation -= 30; } else {self.landTimer += self.instance.runtime.dt;}}
 	getCollision(self, trapInstance){
 		if(!self.dying && (trapInstance.isOpen === true) && trapInstance.objectType.name != "Block" && self.instance.testOverlap(trapInstance) && !trapInstance.inactive) {
 			if (self.trapImmunity.includes(trapInstance.objectType.name) && self.jumpTimer <= 0 && self.landTimer >= 1){
-				self.instance.zElevation += 30; self.jumpTimer = 3; self.landTimer = 0;
-			} else if (!self.trapImmunity.includes(trapInstance.objectType.name) || (self.jumpTimer <= 0 && self.landTimer < 1)) { self.dying = true; self.killer = trapInstance; self.instance.behaviors.MoveTo.moveToPosition(trapInstance.x, trapInstance.y); self.instance.behaviors.MoveTo.rotateSpeed = 0; }
-		}
-	}
-}
-
+				self.instance.zElevation += 30; self.jumpTimer = 3; self.landTimer = 0;} else if (!self.trapImmunity.includes(trapInstance.objectType.name) || (self.jumpTimer <= 0 && self.landTimer < 1)) { self.dying = true; self.killer = trapInstance; self.instance.behaviors.MoveTo.moveToPosition(trapInstance.x, trapInstance.y); self.instance.behaviors.MoveTo.rotateSpeed = 0; }}}}
 class Digger extends Enemy {
 	constructor(enemyName, enemyInstance){
 		super(enemyName, enemyInstance); this.trapImmunity = ["Pitfall", "Trapdoor"]; this.surfaceTimer = 0; this.instance.opacity = 0.5; this.immuneTimer = 0;
@@ -65,8 +48,10 @@ class Digger extends Enemy {
 	getCollision(self, trapInstance){
 		if(!self.dying && (trapInstance.isOpen === true) && trapInstance.objectType.name != "Block" && self.instance.testOverlap(trapInstance) && !trapInstance.inactive) {
 			if (self.trapImmunity.includes(trapInstance.objectType.name) && self.surfaceTimer <= 0 && self.immuneTimer > 4){
-				self.instance.opacity = 1; self.surfaceTimer = 5; self.immobile = true; self.instance.behaviors.MoveTo.stop(); self.immuneTimer = 0;
-			} else if ((self.surfaceTimer > 0) && !self.trapImmunity.includes(trapInstance.objectType.name)) { self.dying = true; self.killer = trapInstance; self.instance.behaviors.MoveTo.moveToPosition(trapInstance.x, trapInstance.y); self.instance.behaviors.MoveTo.rotateSpeed = 0; }
-		}
-	}
+				self.instance.opacity = 1; self.surfaceTimer = 5; self.immobile = true; self.instance.behaviors.MoveTo.stop(); self.immuneTimer = 0;} else if ((self.surfaceTimer > 0) && !self.trapImmunity.includes(trapInstance.objectType.name)) { self.dying = true; self.killer = trapInstance; self.instance.behaviors.MoveTo.moveToPosition(trapInstance.x, trapInstance.y); self.instance.behaviors.MoveTo.rotateSpeed = 0;}}}}
+class Destroyer extends Enemy {
+	constructor(enemyName, enemyInstance){ super(enemyName, enemyInstance); this.destructionLeft = 3; }
+	getCollision(self, trapInstance) { if (self.instance.testOverlap(trapInstance) && self.destructionLeft > 0) { this.destructionLeft -= 1; obstacles.splice(trapInstance.trapIndex, 1); trapInstance.destroy(); } else if(!self.dying && trapInstance.objectType.name != "Block" && (trapInstance.isOpen) && self.instance.testOverlap(trapInstance)) {
+		self.dying = true; self.killer = trapInstance; self.instance.behaviors.MoveTo.moveToPosition(trapInstance.x, trapInstance.y);
+	}}
 }
